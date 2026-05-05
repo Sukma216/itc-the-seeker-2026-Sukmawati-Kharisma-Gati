b5989f04e8418468dbf036512b05810ddb8b68fd
sukma <sukmawatikhrsm216@gmail.com>
2026-05-05 22:20:40 +0700

    upload projeck mobile itc

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'tampilan/homescreen.dart'; 
+
+void main() {
+  runApp(const ITCDirectoryApp());
+}
+
+class ITCDirectoryApp extends StatelessWidget {
+  const ITCDirectoryApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ITC Directory',
+      theme: ThemeData(
+        // Menggunakan warna hijau sesuai tema "The Seeker" di banner
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF1B5E20)),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
---
+++ lib/models/anggota.dart
@@ -0,0 +1,14 @@
+class Member {
+  
+  final String name;
+  final String position;
+  final String division;
+  final String imagePath;
+
+  Member({
+    required this.name,
+    required this.position,
+    required this.division,
+    required this.imagePath,
+  });
+}
